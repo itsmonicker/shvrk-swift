03d4513ca3ac1ee217028275546fed6ad0550153
itsmonicker <m.m.raffaelli@gmail.com>
2015-10-27 01:55:41 -0400

    add conn.php
    file provides db access

---
+++ SHVRK/SHVRK/SHVRK/conn.php
@@ -0,0 +1,12 @@
+/*db access details*/
+
+<?php
+    
+    class Conn {
+        public static $dbhost = "localhost"; /*what goes here?*/
+        public static $dbuser = "<monicamr_0000001>;
+        public static $dbpass = "<Bobo2012>";
+        public static $dbname = " <monicamr_shvrk> ";
+    }
+    
+    ?>
